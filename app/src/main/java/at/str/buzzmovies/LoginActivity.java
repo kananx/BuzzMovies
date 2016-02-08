12d6593c013e032f51ded01e13a51a96729416a0
Gregory Cooke <gcooke3@gatech.edu>
2016-02-08 17:47:02 -0500

    Hard Login works with a toast message for an incorrect login

--- app/src/main/java/at/str/buzzmovies/LoginActivity.java
+++ app/src/main/java/at/str/buzzmovies/LoginActivity.java
@@ -8,6 +8,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.widget.Toast;
+import android.content.Context;
 
 import android.content.CursorLoader;
 import android.content.Loader;
@@ -30,6 +32,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
+import android.content.Intent;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -74,7 +77,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
+                    //attemptLogin();
+                    attemptHardLogin();
                     return true;
                 }
                 return false;
@@ -85,7 +89,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                //attemptLogin();
+                attemptHardLogin();
             }
         });
 
@@ -146,7 +151,55 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 
+    private void attemptHardLogin() {
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
 
+/*
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {*/
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            //showProgress(true);
+            if (email.equals("user@example.com") && password.equals("pass")) {
+                Intent toHomeActivity = new Intent(this, HomeActivity.class);
+                startActivity(toHomeActivity);
+
+            } else {
+                Context context = getApplicationContext();
+                CharSequence text = "Login Incorrect";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+
+
+    }
     /**
      * Attempts to sign in or register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
@@ -207,7 +260,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
         //return password.equals("pass");
-        return password.length() > 4;
+        return password.length() > 3;
     }
 
     /**

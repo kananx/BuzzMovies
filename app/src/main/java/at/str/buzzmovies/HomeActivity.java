ade9f735c48f888377d016d0a2a55b493a85e07d
Thomas Light <tlight6@gatech.edu>
2016-02-08 19:45:33 -0500

    Added logout

--- app/src/main/java/at/str/buzzmovies/HomeActivity.java
+++ app/src/main/java/at/str/buzzmovies/HomeActivity.java
@@ -1,11 +1,13 @@
 package at.str.buzzmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -24,6 +26,19 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button mLogoutButton = (Button) findViewById(R.id.logout_button);
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        Intent toLoginActivity = new Intent(this, LoginActivity.class);
+        startActivity(toLoginActivity);
     }
 
 }

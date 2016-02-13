5ce5cde4e645834538d4be080a03225ad4ac3c97
TieraLee <tieratlee@gmail.com>
2016-02-13 16:18:53 -0500

    add registation screen. make registation button/ gave login screen buttons functionality

--- app/src/main/java/at/str/buzzmovies/LoginActivity.java
+++ app/src/main/java/at/str/buzzmovies/LoginActivity.java
@@ -85,6 +85,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
+
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override

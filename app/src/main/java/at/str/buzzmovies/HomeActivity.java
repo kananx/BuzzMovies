73f5b75dea507f65edc12ab9f8e65099d12bbffe
TieraLee <tieratlee@gmail.com>
2016-02-25 01:12:54 -0500

    add compile dependence to support actoin bar in Manifest, overflow menu started, some color changes

--- app/src/main/java/at/str/buzzmovies/HomeActivity.java
+++ app/src/main/java/at/str/buzzmovies/HomeActivity.java
@@ -6,8 +6,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.app.ActionBar;
+import android.view.Menu;
 
 /**
  * Home activity page that allows the user to interact with available movies.
@@ -21,8 +24,12 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Toolbar action_toolbar = (Toolbar) findViewById(R.id.action_toolbar);
+        setSupportActionBar(action_toolbar);
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +55,15 @@ public class HomeActivity extends AppCompatActivity {
                 profile();
             }
         });
+
+
+
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu overflow) {
+        getMenuInflater().inflate(R.menu.overflow, overflow);
+        return true;
     }
 
     /**

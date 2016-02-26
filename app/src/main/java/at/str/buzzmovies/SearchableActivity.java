9185ec8eb5d6ede0443b2c50d6ad5bb6cc86bdab
Gregory Cooke <gcooke3@gatech.edu>
2016-02-26 14:00:33 -0500

    Fixed some stuff in AndroidManifest

--- app/src/main/java/at/str/buzzmovies/SearchableActivity.java
+++ app/src/main/java/at/str/buzzmovies/SearchableActivity.java
@@ -1,29 +1,50 @@
 package at.str.buzzmovies;
 
-import android.app.SearchManager;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.app.SearchManager;
+
+import static android.support.v4.media.session.MediaButtonReceiver.handleIntent;
 
 public class SearchableActivity extends AppCompatActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.search);
 
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+        //also handle ACTION_SEARCH here
+    }
+
+    private void handleIntent(Intent intent) {
+
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            doMySearch(query);
+            //use the query to search your data somehow
         }
     }
 
-    /**
-     * Performs a search.
-     * @param query the search itself
-     */
-    private void doMySearch(String query) {
 
-    }
 }

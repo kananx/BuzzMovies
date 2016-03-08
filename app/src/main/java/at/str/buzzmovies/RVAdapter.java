47838dff208200edc0d9243ad5868e6fc9203a25
Kanan <kananpatel@gatech.edu>
2016-03-08 03:48:08 -0500

    I cleaned up the variables in RVAdapter, created linear_layout.xml (might be useless idk), and updated searchResultsActivity, content_search_results still has the recyclerview layout,  Going to fix up the add extra string and onclick methods tmrw

--- app/src/main/java/at/str/buzzmovies/RVAdapter.java
+++ app/src/main/java/at/str/buzzmovies/RVAdapter.java
@@ -1,31 +1,59 @@
 package at.str.buzzmovies;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 
 /**
  * Created by Kananx on 3/8/16.
  */
-public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder>{
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.MovieViewHolder>{
 
-    public static class PersonViewHolder extends RecyclerView.ViewHolder {
-        CardView cv;
-        TextView personName;
-        TextView personAge;
-        ImageView personPhoto;
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView year;
 
-        PersonViewHolder(View itemView) {
+        MovieViewHolder(View itemView) {
             super(itemView);
-            cv = (CardView)itemView.findViewById(R.id.cv);
-            personName = (TextView)itemView.findViewById(R.id.person_name);
-            personAge = (TextView)itemView.findViewById(R.id.person_age);
-            personPhoto = (ImageView)itemView.findViewById(R.id.person_photo);
+            title = (TextView)itemView.findViewById(R.id.title);
+            year = (TextView)itemView.findViewById(R.id.year);
         }
+
     }
 
-    ArrayList<Review> reviews;
 
-    RVAdapter(ArrayList<Review> reviews){
-        this.reviews = reviews;
+    ArrayList<Movie> movies;
+    RVAdapter(ArrayList<Movie> movies){
+        this.movies = movies;
     }
+
+    @Override
+    public int getItemCount() {
+        return movies.length;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item, viewGroup, false);
+        MovieViewHolder mvh = new MovieViewHolder(v);
+        return mvh;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder movieViewHolder, int i) {
+        movieViewHolder.title.setText(movies.get(i).title);
+        movieViewHolder.year.setText(movies.get(i).year);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+
 
 }
--- app/src/main/java/at/str/buzzmovies/SearchResultsActivity.java
+++ app/src/main/java/at/str/buzzmovies/SearchResultsActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
@@ -50,6 +52,15 @@ public class SearchResultsActivity extends AppCompatActivity {
             //use the query to search your data somehow
         }
     }
+
+    RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
+    rv.setHasFixedSize(true);
+
+    LinearLayoutManager llm = new LinearLayoutManager(context);
+    rv.setLayoutManager(llm);
+
+    RVAdapter adapter = new RVAdapter(movies);
+    rv.setAdapter(adapter);
 
 
 
